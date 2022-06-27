d43a5d8921de469b8a02b8f5b967bc141ede4291
Önder Alkan <onder.alkan15@gmail.com>
2022-06-27 22:27:22 +0300

    removed cloudinary

--- src/lib/cloudinary-node.ts
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary'
-
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
-    api_key: process.env.CLOUDINARY_APIKEY,
-    api_secret: process.env.CLOUDINARY_SECRETKEY
-});
-
-export default cloudinary;
